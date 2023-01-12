4c7aac3fc7ba4c3fc78a2c3f58c0ca680d4b49a1
Becky139 <rebecca.marriott@outlook.com>
2023-01-12 19:13:50 +0000

    Update settings.py and env.py for dotenv config

--- mutts_cuts/settings.py
+++ mutts_cuts/settings.py
@@ -15,6 +15,11 @@ import os
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
+    from dotenv import load_dotenv
+    load_dotenv()
+
+
+development = os.environ.get('DEVELOPMENT', False)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,9 +33,12 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = development
 
-ALLOWED_HOSTS = ['muttscuts-grooming.herokuapp.com', 'localhost']
+if development:
+    ALLOWED_HOSTS = ['localhost']
+else:
+    ALLOWED_HOSTS = ['muttscuts-grooming.herokuapp.com']
 
 
 # Application definition
@@ -81,16 +89,18 @@ WSGI_APPLICATION = 'mutts_cuts.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-# }
+if development:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
 
 
 # Password validation
