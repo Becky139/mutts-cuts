c683bd2d1bb30c8afd12487e425568fdb0f3f7d9
Becky139 <rebecca.marriott@outlook.com>
2023-01-13 14:04:43 +0000

    Update settings.py and env.py for dotenv config

--- mutts_cuts/settings.py
+++ mutts_cuts/settings.py
@@ -33,7 +33,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['muttscuts-grooming.herokuapp.com', 'localhost']
 
@@ -96,8 +96,6 @@ WSGI_APPLICATION = 'mutts_cuts.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
