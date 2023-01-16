1525d0e8ebb1f704f33311a8109f9f7adce35090
Becky139 <rebecca.marriott@outlook.com>
2023-01-16 20:08:19 +0000

    Add and document profile model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=15, null=False, blank=False)
+    address = models.CharField(max_length=50, null=False, blank=False)
