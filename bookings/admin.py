a9a60117ff32f13361dade6229d5e1d02faded19
Becky139 <rebecca.marriott@outlook.com>
2023-01-17 19:08:41 +0000

    Update booking and service admin files

--- bookings/admin.py
+++ bookings/admin.py
@@ -2,5 +2,4 @@ from django.contrib import admin
 from .models import Booking  # from current directory model file import Booking model
 
 # Register your models here.
-
 admin.site.register(Booking)
--- services/admin.py
+++ services/admin.py
@@ -2,5 +2,4 @@ from django.contrib import admin
 from .models import Services  # from current directory model file import Services model
 
 # Register your models here.
-
 admin.site.register(Services)
