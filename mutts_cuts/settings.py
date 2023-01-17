95207b670b3ca8a1a5e4d81e9baee79dc0d17c16
Becky139 <107041779+Becky139@users.noreply.github.com>
2023-01-17 13:44:58 +0000

    Commit unresolved allauth form extension

--- mutts_cuts/settings.py
+++ mutts_cuts/settings.py
@@ -104,6 +104,10 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+# Bespoke signup form
+
+# ACCOUNT_FORMS = {'signup': 'profiles.forms.MyCustomSignupForm'}
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
