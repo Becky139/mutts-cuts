f948f8ece992776afad6e753bbbdae7ff4c20017
Becky139 <rebecca.marriott@outlook.com>
2023-01-20 14:13:09 +0000

    Add cancel_booking method to booking views

--- bookings/views.py
+++ bookings/views.py
@@ -48,3 +48,9 @@ def edit_booking(request, booking_id):
         'form': form
     }
     return render(request, 'bookings/edit_booking.html', context)
+
+
+def cancel_booking(request, booking_id):
+    booking = get_object_or_404(Booking, id=booking_id)
+    booking.delete()
+    return redirect('view_booking')
