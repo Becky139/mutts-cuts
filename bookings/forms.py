f3b8e740489f1f68399c33ea03bbc2babba7af81
Becky139 <rebecca.marriott@outlook.com>
2023-01-24 09:36:46 +0000

    Remove user from booking for. Not required

--- bookings/forms.py
+++ bookings/forms.py
@@ -5,5 +5,5 @@ from .models import Booking
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
-        fields = ['user', 'pet_name', 'date', 'start_time', 'service_type']
+        fields = ['pet_name', 'date', 'start_time', 'service_type']
  
