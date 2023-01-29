cfec35b824de5905e70af87173f706fe69153cea
Becky139 <rebecca.marriott@outlook.com>
2023-01-29 15:21:20 +0000

    fixing booking errors

---
+++ services/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Service
+
+
+def services(request):
+    """
+    Services view
+    """
+    services = Service.objects.all()
+    context = {
+        'services': services
+    }
+    return render(request, 'services/services.html', context)
