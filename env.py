429ad19812d99f7c88ea4938448be696164034cf
Becky139 <rebecca.marriott@outlook.com>
2023-01-29 10:37:15 +0000

    remove services app

--- env.py
+++ env.py
@@ -2,5 +2,4 @@ import os
 
 os.environ['DATABASE_URL'] = 'postgres://pbikktrz:3GdOF8Cug4tABt93j48r1YgQXfbIq4Ac@kandula.db.elephantsql.com/pbikktrz'
 os.environ['SECRET_KEY'] = 'my_super^secret@key'
-os.environ['CLOUDINARY_URL'] = 'cloudinary://342747126591662:a7GlvV_47dnYOnwWNV6fxs5OuYE@dxw78a58q'
 os.environ['DEVELOPMENT'] = 'True'
